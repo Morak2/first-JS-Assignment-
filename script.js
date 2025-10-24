c63852be23095ccf2cf29f27ccd75e6c4ffe5b59
unknown <morakinyosolomonakinyele@gmail.com>
2025-10-24 22:53:42 +0100

    first js ass

---
+++ script.js
@@ -0,0 +1,16 @@
+let letter=prompt('Type in your Single Character');
+if(letter=='a'|| letter=='e' || letter=='i' || letter=='o' || letter=='u'){
+    console.log('This is a vowel');
+
+}else if(letter=='b' || letter=='c' || letter=='d' || letter=='f' || letter=='g' || letter=='h' || letter=='j' || letter=='k' || letter=='l' || letter=='m' || letter=='n' || letter=='p' || letter=='q' || letter=='r' || letter=='s' || letter=='t' || letter=='v' || letter=='w' || letter=='x' || letter=='y' || letter=='z'){
+    console.log('This is a consonant');
+
+}else if(letter=='0' || letter=='1' || letter=='2' || letter=='3' || letter=='4' || letter=='5' || letter=='6' || letter=='7' || letter=='8' || letter=='9'){
+    console.log('This is a number');
+
+}else if(letter=='`' || letter=='~' || letter=='!' || letter=='@' || letter=='#' || letter=='$' || letter=='%' || letter=='^' || letter=='&' || letter=='*' || letter=='(' || letter==')' || letter=='-' || letter=='_' || letter=='=' || letter=='+' || letter=='[' || letter==']' || letter=='{' || letter=='}' || letter=='|' || letter=='/' || letter=='"' || letter==';' || letter==':' || letter=='?' || letter=='.' || letter=='>' || letter==',' || letter=='<'){
+    console.log('This is a Special Character');
+
+}else{
+    console.log('I do not know what i am')
+};
